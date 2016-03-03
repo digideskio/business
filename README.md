db8d1106762a56d98843921e0af67c61a96d938e
russellgoldenberg <russellgoldenberg@gmail.com>
2016-03-03 12:57:59 -0500

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Business
+
+* After making changes, run `npm version [patch, minor, major]`
+* Then `gulp -u username` to bundle and deploy
+
+File lives at https://apps.bostonglobe.com/js/business/
--- gulp-tasks/ssh.js
+++ gulp-tasks/ssh.js
@@ -3,10 +3,12 @@ const shell 	= require('shelljs');
 const argv		= require('yargs').argv;
 const path		= '/web/bgapps/html/common/js/business';
 const host		= 'shell.boston.com';
+const version	= require ('../package.json').version;
 
 gulp.task('ssh', function(cb) {
 	if (argv.u) {
-		const command = `scp -r dist/. ${argv.u}'@${host}:${path}`
+		const command = `(cd dist; scp -r business-${version}.js ${argv.u}@${host}:${path})`
+		console.log(command)
 		shell.exec(command, cb);	
 	} else {
 		console.log('enter -u username to upload')
